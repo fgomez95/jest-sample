fdd60ddb69603ada04644057fa33420aeb800566
fgomez95 <fgomez_sierra95@gmail.com>
2019-05-04 21:56:23 -0600

    refactored db and added function tests

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-const db = [
-    "luctus.felis@fermentumarcu.ca",
-    "pharetra.ut.pharetra@necurnaet.com",
-    "rutrum.Fusce.dolor@nibh.ca",
-    "luctus@euismod.net",
-    "ornare.elit@lectusante.org",
-    "Nunc@accumsan.com",
-    "interdum@ad.ca",
-    "odio.vel@nibhvulputate.org",
-    "augue@justofaucibuslectus.net",
-    "eget@urnaNullam.edu",
-    "Donec.felis@aptenttacitisociosqu.co.uk",
-    "erat@arcuMorbisit.org",
-    "ut.pellentesque@fermentum.org",
-    "laoreet@PhasellusornareFusce.org",
-    "diam.at.pretium@sapien.com",
-    "erat.nonummy.ultricies@Nullaeuneque.edu",
-    "arcu.Sed.et@venenatis.com"
-];
+var db = require('./dbMock');
 
 const dbSearch = (searchInput, database) => {
     const searchResult = database.filter(el => el.includes(searchInput));
     return searchResult.length > 3 ? searchResult.slice(0, 3) : searchResult;
 };
 
-console.log(dbSearch('uc', db));
+//console.log(dbSearch('uc', db));
 
+module.exports = dbSearch;
--- index.test.js
+++ index.test.js
@@ -1,3 +1,22 @@
-it('should pass', ()=>{
-    expect(true).toBe(true);
+var dbSearch = require('./index');
+var db = require('./dbMock');
+
+describe('dbSearch', () => {
+    it('should be imported correctly', () => {
+        expect(dbSearch).toBeDefined();
+    });
+    it('should return an empty array if no result found', () => {
+        const result = dbSearch('asdfsdf', db);
+        expect(result).toEqual([]);
+    });
+    it('should find one result', () => {
+        const emailInput = 'luctus.felis@fermentumarcu.ca';
+        const result = dbSearch(emailInput, db);
+        expect(result[0]).toEqual(emailInput);
+    });
+    it('should return a max results length of 3', () => {
+        const resLength = dbSearch('.com', db).length;
+        const lengthFlag = resLength <= 3 && resLength > 0;
+        expect(lengthFlag).toBe(true);
+    });
 });
